7ab392143e0e6246e1f7d37c557f5c88e0e8bcda
anggitprastika <anggitprastikas1@gmail.com>
2026-01-12 14:10:34 +0700

    Day 6: navigation test with click and URL Verification

---
+++ tests/navigation.spec.js
@@ -0,0 +1,15 @@
+import{test,expect} from '@playwright/test';
+
+test('Navigate to Docs page from homepage', async({page})=>{
+//Step 1: Open homepage
+await page.goto('https://playwright.dev/');
+
+//Step 2: Click "Docs" link
+await page.getByRole('link',{name:'Docs'}).click();
+
+//Step 3: Verify URL contains /docs
+await expect(page).toHaveURL(/.*docs*/);
+
+
+
+});
