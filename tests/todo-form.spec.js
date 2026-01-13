1ffcd18c22aa45080c0df112d7cc36726c4a03af
anggitprastika <anggitprastikas1@gmail.com>
2026-01-13 10:26:00 +0700

    Day 11A: add multiple test cases using POM

--- tests/todo-form.spec.js
+++ tests/todo-form.spec.js
@@ -1,12 +1,27 @@
 import { test, expect } from '@playwright/test';
 import { TodoPage } from '../pages/TodoPage';
 
-test('Add todo using POM', async ({ page }) => {
-  const todoPage = new TodoPage(page);
+test.describe('Todo App - using POM', () => {
 
-  await todoPage.goto();
-  await todoPage.addTodo('Belajar Playwright Day 10');
+  test('Add single todo', async ({ page }) => {
+    const todoPage = new TodoPage(page);
 
-  const todoItem = await todoPage.getLastTodo();
-  await expect(todoItem).toHaveText('Belajar Playwright Day 10');
+    await todoPage.goto();
+    await todoPage.addTodo('Belajar Playwright Day 11');
+
+    const todoItem = await todoPage.getLastTodo();
+    await expect(todoItem).toHaveText('Belajar Playwright Day 11');
+  });
+
+  test('Add multiple todos', async ({ page }) => {
+    const todoPage = new TodoPage(page);
+
+    await todoPage.goto();
+    await todoPage.addTodo('Todo pertama');
+    await todoPage.addTodo('Todo kedua');
+
+    const lastTodo = await todoPage.getLastTodo();
+    await expect(lastTodo).toHaveText('Todo kedua');
+  });
+
 });
